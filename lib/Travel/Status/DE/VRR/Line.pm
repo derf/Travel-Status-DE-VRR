e976f7f8fb11ff20486645c198f7e4d7eafb653b
Daniel Friesel <derf@finalrewind.org>
2013-12-18 16:33:38 +0100

    version bump

--- lib/Travel/Status/DE/VRR/Line.pm
+++ lib/Travel/Status/DE/VRR/Line.pm
@@ -6,7 +6,7 @@ use 5.010;
 
 use parent 'Class::Accessor';
 
-our $VERSION = '1.04';
+our $VERSION = '1.05';
 
 Travel::Status::DE::VRR::Line->mk_ro_accessors(
 	qw(direction name operator route type valid));
@@ -46,7 +46,7 @@ requested station
 
 =head1 VERSION
 
-version 1.04
+version 1.05
 
 =head1 DESCRIPTION
 
