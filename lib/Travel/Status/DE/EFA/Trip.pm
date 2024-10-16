0e1e9168e74981aa9f121cc269fa7010fefac62f
Birte Kristina Friesel <derf@finalrewind.org>
2024-10-16 02:52:00 +0200

    Release v3.02

--- lib/Travel/Status/DE/EFA/Trip.pm
+++ lib/Travel/Status/DE/EFA/Trip.pm
@@ -9,7 +9,7 @@ use Travel::Status::DE::EFA::Stop;
 
 use parent 'Class::Accessor';
 
-our $VERSION = '3.01';
+our $VERSION = '3.02';
 
 Travel::Status::DE::EFA::Trip->mk_ro_accessors(
 	qw(operator product product_class name line number type id dest_name dest_id)
@@ -143,7 +143,7 @@ trip
 
 =head1 VERSION
 
-version 3.01
+version 3.02
 
 =head1 DESCRIPTION
 
